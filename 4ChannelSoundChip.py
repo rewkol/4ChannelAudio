568f279441817abfdeafcfe74fdbfb401415b851
Mitchell Golding <MitchellWGolding@gmail.com>
2018-01-06 11:57:16 -0400

    Added Volume to Backend
    Added Volume to Backend
    
    Most backend work should be done now, all that remains is the hard part, which is mapping all the technical inside stuff to an easy to use outside stuff

--- 4ChannelSoundChip.py
+++ 4ChannelSoundChip.py
@@ -1,8 +1,11 @@
 import random, wave, struct, math, time
 
-def generate(freq, kind,  num, samples):
+def generate(freq, volume, kind,  num, samples):
     toret = []
-
+    if volume < 0:
+        volume = 0
+    elif volume > 16383:
+        volume = 16383
     if kind == 1:
         #White Noise
         for i in range(num):
@@ -11,21 +14,21 @@ def generate(freq, kind,  num, samples):
         #Sawtooth 
         for i in range(num): 
             osc = samples/freq
-            value = 14000 * (i%osc)*(1/osc)
+            value = 2*volume * (i%osc)*(1/osc)
             toret.append(struct.pack("h",int(value)))
     elif kind == 3:
         #Sine Wave
         for i in range(num):
-            value = 14000 * (math.sin(6.283*freq*(i/44100)))
+            value = 2*volume * (math.sin(6.283*freq*(i/44100)))
             toret.append(struct.pack("h",int(value)))
     elif kind == 4:
         #Squaretooth
         for i in range(num):
             osc = samples/freq
             if (i%osc)*2<osc:
-                value = 7000
+                value = volume
             else:
-                value = -7000
+                value = -volume
             toret.append(struct.pack("h",int(value)))
     
     return toret
@@ -37,7 +40,7 @@ def writes(samples):
     remaining = samples
     while remaining > 0:
         gennum = random.randrange(5512,44101,5512)#Decides how many samples are generated per loop
-        gen = generate(random.randrange(440,880,10), 4, gennum*2, samples)#Does random tones each loop. For programmed songs don't loop and write each gen out
+        gen = generate(random.randrange(440,880,10), random.randrange(0,16384,1), 2, gennum*2, samples)#Does random tones each loop. For programmed songs don't loop and write each gen out
         for byte in gen:
             wav.writeframesraw(byte)
         remaining -= gennum
